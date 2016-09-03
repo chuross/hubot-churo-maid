4f3e7fafbfdf53b1234745450b654beee9161db8
chuross <daisuke_ito@dwango.co.jp>
2016-09-03 19:50:16 +0900

    コマンドのオプションを処理するクラスを実装

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,4 +1,7 @@
 /*
+* Description:
+*   基本機能いろいろ
+*
 * Commands:
 *   おはよう - 朝の挨拶
 */
@@ -6,5 +9,5 @@ export default function(robot) {
 
   robot.respond(/おはよう/, msg => {
     msg.send('おはようございます！');
-  })
+  });
 }
