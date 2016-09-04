3c3c846d8a669bec0be126389d9e79dd8bb66922
chuross <azarasi.mo.mo@gmail.com>
2016-09-04 13:57:21 +0900

    細かい修正

--- scripts/todo_github.js
+++ scripts/todo_github.js
@@ -7,9 +7,9 @@ import { Utils } from './Utils'
 * Description:
 *   GithubによるTodo管理
 * Commands:
-*   todo [-today] - タスク一覧を表示
-*   todo create :title [-l=優先度低, -h=優先度高] - 指定タイトルでタスクを作る
-*   todo done :issue_id - 指定IDのタスクを終了する
+*   todo list [-a 全部表示, -n 名前] - タスク一覧を表示
+*   todo create :title [-l 優先度低, -n 名前] - 指定タイトルでタスクを作る
+*   todo done :issue_id [-n 名前] - 指定IDのタスクを終了する
 */
 const githubUrl = process.env.HUBOT_MAID_GITHUB_URL;
 const githubToken = process.env.HUBOT_MAID_GITHUB_TOKEN;
@@ -17,21 +17,24 @@ const githubHeader = { headers: { Authorization: `token ${githubToken}` } }
 
 export default function(robot) {
 
-  robot.respond(/todo|今日の仕事は？/i, msg => {
+  robot.respond(/todo list|今日の仕事/i, msg => {
     const params = OptionParser.parse([
       ['-a', '--all'],
       ['-n', '--name STRING']
     ], msg.message.text.split(' '));
 
     const userName = params.name || msg.message.user.name;
+    const query = params.all ? {} : { labels: 'high' }
 
     const url = `${githubUrl}/repos/${userName}/todo/issues`;
-    axios.get(url).then(response => {
-        msg.send('今日の仕事はこちらになります');
-        response.data.forEach(issue => {
-          msg.send(`#${issue.number} *<${issue.url}|${issue.title}>*`);
-        });
-      }).catch(Utils.error(msg));
+    axios.get(url, {
+      params: query
+    }).then(response => {
+      msg.send('残りの仕事です');
+      response.data.forEach(issue => {
+        msg.send(`#${issue.number} *<${issue.url}|${issue.title}>*`);
+      });
+    }).catch(Utils.error(msg));
   });
 
   robot.respond(/todo create (.*)/i, msg => {
@@ -39,19 +42,17 @@ export default function(robot) {
 
     const params = OptionParser.parse([
       ['-n', '--name STRING'],
-      ['-p', '--priority STRING'],
-      ['-d', '--doing']
+      ['-l', '--low'],
     ], text.split(' '));
 
     const title = text.split(' ')[3];
     const userName = params.name || msg.message.user.name;
-    const priority = params.priority || 'high';
-    const labels = params.doing ? [priority, 'doing'] : [priority];
+    const priority = params.low ? 'low' : 'high';
 
     const url = `${githubUrl}/repos/${userName}/todo/issues`;
     axios.post(url, {
       title: title,
-      labels: labels
+      labels: [priority]
     }, githubHeader).then(response => {
       const issue = response.data;
       msg.send('仕事を追加しました！');
