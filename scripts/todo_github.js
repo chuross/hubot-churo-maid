f25904987841f9c786cedd66b80bbc3e828f4aa3
chuross <azarasi.mo.mo@gmail.com>
2016-09-04 13:04:50 +0900

    タスク追加できるようにした

--- scripts/todo_github.js
+++ scripts/todo_github.js
@@ -1,5 +1,6 @@
-import { OptionParser } from './optionParser'
 import axios from 'axios'
+import { OptionParser } from './optionParser'
+import { Utils } from './Utils'
 
 
 /**
@@ -11,25 +12,51 @@ import axios from 'axios'
 *   todo done :issue_id - 指定IDのタスクを終了する
 */
 const githubUrl = process.env.HUBOT_MAID_GITHUB_URL;
+const githubToken = process.env.HUBOT_MAID_GITHUB_TOKEN;
 
 export default function(robot) {
 
-  robot.respond(/todo|todo (.*)/i, msg => {
+  robot.respond(/todo|今日の仕事は？/i, msg => {
     const params = OptionParser.parse([
       ['-a', '--all'],
       ['-n', '--name STRING']
     ], msg.message.text.split(' '));
 
-    const targetUser = params.name || msg.message.user.name;
+    const userName = params.name || msg.message.user.name;
 
-    const url = `${githubUrl}/repos/${targetUser}/todo/issues`;
-    axios.get(url)
-      .then(response => {
-        msg.send('仕事はこちらです！');
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    const url = `${githubUrl}/repos/${userName}/todo/issues`;
+    axios.get(url).then(response => {
+        msg.send('今日の仕事はこちらになります');
+        response.data.forEach(issue => {
+          msg.send(`#${issue.number} *<${issue.url}|${issue.title}>*`);
+        });
+      }).catch(Utils.error(msg));
+  });
+
+  robot.respond(/todo create (.*)/i, msg => {
+    const text = msg.message.text;
+
+    const params = OptionParser.parse([
+      ['-n', '--name STRING'],
+      ['-p', '--priority STRING'],
+      ['-d', '--doing']
+    ], text.split(' '));
+
+    const title = text.split(' ')[3];
+    const userName = params.name || msg.message.user.name;
+    const priority = params.priority || 'high';
+    const labels = params.doing ? [priority, 'doing'] : [priority];
+
+    const url = `${githubUrl}/repos/${userName}/todo/issues`;
+    axios.post(url, {
+      title: title,
+      labels: labels
+    }, {
+      headers: {
+        Authorization: `token ${githubToken}`
+      }
+    }).then(response => {
+      msg.send('仕事を追加しました！');
+    }).catch(Utils.error(msg));
   });
 }
