27cd7875beaf4768c047df55192b4a804a979827
chuross <azarasi.mo.mo@gmail.com>
2016-09-09 21:38:24 +0900

    引数を扱いやすいように変更した

--- scripts/Utils.js
+++ scripts/Utils.js
@@ -8,4 +8,11 @@ export class Utils {
       `);
     }
   }
+
+  static argString(robot, task, text) {
+    const filter = [robot.name, ...task.split(' ')]
+    return text.split(' ').filter(item => {
+      return !filter.includes(item);
+    });
+  }
 }
