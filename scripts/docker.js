df7d57d6a9b468348db2bc3e508f64307c66bf0a
chuross <daisuke_ito@dwango.co.jp>
2016-09-11 16:47:04 +0900

    Dockerコンテナを起動できるようにした

--- scripts/docker.js
+++ scripts/docker.js
@@ -1,4 +1,4 @@
-import process from 'child-process-es6-promise'
+import childProcess from 'child-process-es6-promise'
 import args from 'minimist'
 import { Utils } from './Utils'
 
@@ -8,15 +8,16 @@ import { Utils } from './Utils'
 * Commands:
 *   docker ps - 起動中のコンテナを表示する
 *     --all 全て表示する
-*   docker run :image - イメージを立ち上げる
+*   docker run :image|:giturl - イメージを立ち上げる
 *     --tmp 1日だけ立ち上げる
 *     --detach 起動時にホストを表示しない
+*     --git
 *   docker halt :containerID - 指定したコンテナを落とす(停止・削除)
-*   docker build :gitUrl
-*     --tmp 1日だけ立ち上げる
-*     --detach 起動時にホストを表示しない
 *   docker clean - 起動していないコンテナを全て削除
 */
+const containerId = process.env.HOSTNAME;
+const hostUrl = process.env.HUBOT_MAID_HOST_URL;
+
 export default function(robot) {
 
   robot.respond(/docker ps/, msg => {
@@ -24,10 +25,10 @@ export default function(robot) {
       alias: {
         a: 'all'
       }
-    })
+    });
 
     msg.send(`${params.all ? '全ての' : ''}起動中のDockerコンテナを取得しますね`);
-    process.exec(`docker ps ${paramas.all ? '-a' : ''}`)
+    childProcess.exec(`docker ps ${params.all ? '-a' : ''} | grep -v "${containerId}"`)
       .then(result => {
         msg.send(result.stdout);
       })
@@ -35,4 +36,35 @@ export default function(robot) {
         msg.send(error);
       });
   });
+
+  robot.respond(/docker run (.+)/, msg => {
+    const params = args(Utils.argString(robot, 'docker run', msg.message.text), {
+      alias: {
+        d: 'detach'
+      }
+    });
+
+    const imageName = params['_'][0];
+    msg.send(`${imageName}を起動します`);
+
+    let command = 'docker run -P -d';
+
+    const containerNameBase = `${msg.message.user.name}_${imageName.replace(/\//, '_')}`;
+    const containerName = params.tmp ? `tmp_${containerNameBase}` : containerNameBase;
+    command = command.concat(` --name ${containerName}`);
+
+    childProcess.exec(`${command} ${imageName}`)
+      .then(() => childProcess.exec(`docker port ${containerName}`))
+      .then(result => {
+        msg.send('コンテナを起動しました！');
+        if (!params.detach) {
+          const port = result.stdout.match(/:[0-9]+/);
+          const url = `${hostUrl}${port}`;
+          msg.send(`接続先のURLはこちらです ${url}`);
+        }
+      })
+      .catch(error => {
+        msg.send(error);
+      });
+  })
 }
